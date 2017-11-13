af7ead29d45646b57258f5c5ba2e9579ebc9c7cd
APPDEVCOUNT <sira.deen@gmail.com>
2017-11-13 10:54:22 +0300

    Add project files.

---
+++ XForms/XForms/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XForms.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XForms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //if (Device.RuntimePlatform == Device.iOS)
+            //    MainPage = new MainPage();
+            //else
+            //    MainPage = new NavigationPage(new MainPage());
+
+            if (Device.RuntimePlatform == Device.iOS)
+                MainPage = new Home();
+            else
+                MainPage = new NavigationPage(new Home());
+
+        }
+    }
+}
