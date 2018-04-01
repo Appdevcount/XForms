5baf972d6c617e349fb0bb861afb79c3ab1854e5
appdevcount <sira.deen@gmail.com>
2018-04-01 23:07:07 +0300

    H-

---
+++ XForms/XForms/Views/SfControls/SfMultiControlTabPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XForms.Views.SfControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SfMultiControlTabPage : ContentPage
+    {
+        public SfMultiControlTabPage()
+        {
+            InitializeComponent();
+            //var tabView = new SfTabView();
+            //var allContactsGrid = new Grid { BackgroundColor = Color.Red };
+            //var favoritesGrid = new Grid { BackgroundColor = Color.Green };
+            //var contactsGrid = new Grid { BackgroundColor = Color.Blue };
+            //var tabItems = new TabItemCollection
+            //{
+            //new SfTabItem()
+            //    {
+            //    Title = "Calls",
+            //    Content = allContactsGrid
+            //    },
+            //new SfTabItem()
+            //    {
+            //    Title = "Favorites",
+            //    Content = favoritesGrid
+            //    },
+            //new SfTabItem()
+            //    {
+            //    Title = "Contacts",
+            //    Content = contactsGrid
+            //    }
+            //};
+
+            //tabView.Items = tabItems;
+            //this.Content = tabView;
+        }
+    }
+}
